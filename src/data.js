611a93325ee5d30699f30224a3145081e4755b2f
SolidDogor <olivosralejandromatias@gmail.com>
2026-01-06 11:33:02 -0500

    Actualizar formato de imagenes

--- src/data.js
+++ src/data.js
@@ -1,21 +1,33 @@
 //Presentation
+import githubImage from './assets/Presentation/github.svg';
 import instagramImage from './assets/Presentation/instagram.svg';
 import linkedinImage from './assets/Presentation/linkedin.svg';
-import githubImage from './assets/Presentation/github.svg';
 
 //Projects
 import amoresDulcesImage from './assets/Projects/logo-amores-dulces.webp';
 
-//Skills
+//Technologies
+import canvaImage from './assets/Technologies/canva.svg';
 import cppImage from './assets/Technologies/cpp.svg';
-import javaImage from './assets/Technologies/java.svg';
-import golangImage from './assets/Technologies/golang.svg';
-import javascriptImage from './assets/Technologies/javascript.svg';
-import htmlImage from './assets/Technologies/html.svg';
 import cssImage from './assets/Technologies/css.svg';
+import figmaImage from './assets/Technologies/figma.svg';
+import gimpImage from './assets/Technologies/gimp.svg';
+import golangImage from './assets/Technologies/golang.svg';
+import htmlImage from './assets/Technologies/html.svg';
+import javaImage from './assets/Technologies/java.svg';
+import javascriptImage from './assets/Technologies/javascript.svg';
+import mongoImage from './assets/Technologies/mongodb.svg';
+import mysqlImage from './assets/Technologies/mysql.svg';
+import odooImage from './assets/Technologies/odoo.svg';
+import pythonImage from './assets/Technologies/python.svg';
 import reactImage from './assets/Technologies/react.svg';
 
 export const SOCIAL_DATA = [
+  {
+    url: 'https://github.com/SolidDogor',
+    imagePath: githubImage,
+    social: 'github',
+  },
   {
     url: 'https://www.instagram.com/alejandr0lirod',
     imagePath: instagramImage,
@@ -25,11 +37,6 @@ export const SOCIAL_DATA = [
     url: 'https://linkedin.com/in/alejandro-matias-olivos-rodriguez-95201b107',
     imagePath: linkedinImage,
     social: 'linkedin',
-  },
-  {
-    url: 'https://github.com/SolidDogor',
-    imagePath: githubImage,
-    social: 'github',
   },
 ];
 
@@ -44,31 +51,79 @@ export const PROJECTS_DATA = [
 
 export const TECHNOLOGIES_DATA = [
   {
-    imagePath: cppImage,
-    technologieName: 'C++',
+    title: 'Core Programming',
+    images: [
+      {
+        imagePath: cppImage,
+        technologieName: 'C++',
+      },
+      {
+        imagePath: javaImage,
+        technologieName: 'Java',
+      },
+      {
+        imagePath: golangImage,
+        technologieName: 'GO',
+      },
+      {
+        imagePath: pythonImage,
+        technologieName: 'Python',
+      }
+    ]
   },
   {
-    imagePath: javaImage,
-    technologieName: 'Java',
+    title: 'Desarrollo Full-Stack',
+    images: [
+      {
+        imagePath: htmlImage,
+        technologieName: 'HTML',
+      },
+      {
+        imagePath: cssImage,
+        technologieName: 'CSS',
+      },
+      {
+        imagePath: javascriptImage,
+        technologieName: 'Javascript',
+      },
+      {
+        imagePath: reactImage,
+        technologieName: 'React',
+      },
+      {
+        imagePath: mongoImage,
+        technologieName: 'MongoDB',
+      },
+      {
+        imagePath: mysqlImage,
+        technologieName: 'MySql',
+      },
+    ]
   },
   {
-    imagePath: golangImage,
-    technologieName: 'GO',
+    title: 'Soluciones Empresariales',
+    images: [
+      {
+        imagePath: odooImage,
+        technologieName: 'ODOO',
+      },
+    ]
   },
   {
-    imagePath: javascriptImage,
-    technologieName: 'JavaScript',
-  },
-  {
-    imagePath: htmlImage,
-    technologieName: 'HTML',
-  },
-  {
-    imagePath: cssImage,
-    technologieName: 'CSS',
-  },
-  {
-    imagePath: reactImage,
-    technologieName: 'React',
+    title: 'Creatividad y Diseño',
+    images: [
+      {
+        imagePath: canvaImage,
+        technologieName: 'Canva',
+      },
+      {
+        imagePath: figmaImage,
+        technologieName: 'Figma',
+      },
+      {
+        imagePath: gimpImage,
+        technologieName: 'Gimp',
+      },
+    ]
   },
 ];
