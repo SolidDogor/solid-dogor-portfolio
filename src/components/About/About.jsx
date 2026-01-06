aa6c7e5f00354ee5e7ac0356f61c42e761ebc538
SolidDogor <olivosralejandromatias@gmail.com>
2026-01-06 00:52:29 -0500

    Actualizar estilos

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,30 @@
 import './About.css';
+import Card from '../Card/Card';
+import RonProgramador from '../../assets/About/RonProgramador.gif';
 
 export default function About() {
-    return (
-        <div className='about'>
-            Por implementar sobre mi.
+  return (
+    <div className="about-section">
+      <h1 className="title">Más allá del código...</h1>
+      <div className="about-information">
+        <p className="about-description">
+            Estudiante de Ingeniería de Software en la USIL, apasionado por la lógica
+            y el desarrollo de sistemas. Mi flujo de trabajo suele estar supervisado por mi
+            gato Ron. Fuera del código, exploro mi creatividad a través de la música y los
+            videojuegos. Además, comparto mis conocimientos como profesor de Alemán, Inglés
+            y Matemáticas.
+        </p>
+        <div className="about-right">
+            <div className="about-skills">
+                <Card as="h2" title="Abitur alemán"/>
+                <Card as="h2" title="B2 en inglés"/>
+            </div>
+            <div className="code-ron">
+                <img src={RonProgramador} />
+                <p className='code-ron'>Ron programando...</p>
+            </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,26 @@
 import './Card.css';
 
-export default function Card({ imagePath, title, description, url }) {
+export default function Card({
+  imagePath,
+  title,
+  description,
+  url,
+  as: Heading = 'h1'
+}) {
   return (
     <a href={url} className="card-link">
       <section className="card">
-        <div className="card-image">
-          <img src={imagePath} alt={title} className="card-image" />
-        </div>
+        {imagePath && (
+          <div className="card-image">
+            <img src={imagePath} alt={title} className="card-image__img" />
+          </div>
+        )}
+
         <div className="card-information">
-          <h1 className="card-name">{title}</h1>
-          <p className="card-description">{description}</p>
+          <Heading className="card-name">{title}</Heading>
+          {description && (
+            <p className="card-description">{description}</p>
+          )}
         </div>
       </section>
     </a>
